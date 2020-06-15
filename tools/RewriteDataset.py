77659aa44e6a86e0e00a6c72457307bfdb9efb0e
181220014-Gao Chenxiao <1910342119@qq.com>
2020-06-15 21:45:49 +0800

    gcx: split GPS dataset

--- tools/RewriteDataset.py
+++ tools/RewriteDataset.py
@@ -7,9 +7,13 @@ import time
 import csv
 import matplotlib.pyplot as plt
 import math
-
-prefix = "./train/processed/"
+import pandas as pd
 from tools.GetRoadID import getRoadID
+
+GPS1 = "./asset/20191201_20191220.csv"
+GPS2 = "./asset/201901_201903.csv"
+GPS3 = "./asset/201910_11.csv"
+
 
 def rewriteDataset():
     count = 0
@@ -77,8 +81,63 @@ def rewriteDataset():
     except :
         print("successfully processed {} lines".format(count))
 
+def rewrite_dataset():
+    dates = set()
+    entries = dict()
+    count = 0
+    ok = 1
+    try:
+        with open(GPS1, "r") as fp:
+            line = fp.readline()
+            while line:
+                count += 1
+                # if count==1000:
+                #     assert False
+                for char in ["\"", "[", "]"]:
+                    line = line.replace(char, "")
+                gps_records = line.split(",")[2:]
+
+                # 确定日期
+                lng, lat, speed, direction, seconds = [eval(i) for i in gps_records[0].strip().split(" ")]
+                time_stamp = time.gmtime(seconds)
+                date = str(time_stamp[1])+str(time_stamp[2])
+                if date not in dates:
+                    dates.add(date)
+                    entries[date] = []
+
+                df = [[eval(i) for i in j.strip().split(" ")] for j in gps_records]
+                df = pd.DataFrame(data=df, columns=["lng", "lat", "speed", "direction", "seconds"])
+                df["road_id"] = df.apply(lambda x:getRoadID(x["lng"], x["lat"], x["direction"]), axis=1)
+                df["time"] = df.apply(lambda x:time.gmtime(x["seconds"])[3:6], axis=1)
+
+                df = df[df["road_id"]!=0][["road_id", "speed", "time"]]
+
+                if df.shape[0] <= 2:
+                    line = fp.readline()
+                    continue
+
+                entries[date].append(df.values.tolist())
+
+                for d in dates:
+                    if len(entries[d]) == 1024:
+                        with open("./train/201912/"+d+".csv", "a", newline="") as file:
+                            writer = csv.writer(file)
+                            writer.writerows(entries[d])
+                            entries[d] = []
+                            print("wirte into", d, ".csv")
+
+                line = fp.readline()
+    except:
+        print("successfully processed {} lines".format(count))
+        for d in dates:
+            print(len(entries[d]))
+
+
+
+
 
 
 if __name__ == "__main__":
-    rewriteDataset()
+    # rewriteDataset()
     # showSpeed()
+    rewrite_dataset()
